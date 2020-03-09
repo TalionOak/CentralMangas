0ff020a1890bb245ed4c3b3c7160219c5ad43259
Ailton Silva <ailtonsscarvalho@gmail.com>
2020-03-09 00:43:50 -0300

    Add files via upload

---
+++ PaginaVulcanNovel.cs
@@ -0,0 +1,46 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace Teste
+{
+    public class PaginaVulcanNovel
+    {
+        private SeleniumConfigurations _configurations;
+        private IWebDriver _driver;
+
+        public PaginaVulcanNovel(SeleniumConfigurations seleniumConfigurations)
+        {
+            _configurations = seleniumConfigurations;
+            ChromeOptions optionsFF = new ChromeOptions();
+            optionsFF.AddArgument("--headless");
+
+            _driver = new ChromeDriver(_configurations.CaminhoDriverChrome, optionsFF);
+        }
+
+        public void CarregarPagina()
+        {
+            _driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(_configurations.Timeout);
+            _driver.Navigate().GoToUrl(_configurations.UrlPagina);
+        }
+
+
+        //public string ObterNovel()
+        //{
+        //    var novel = _driver.FindElement(By.ClassName("entry-content"));
+        //    return novel.Text;
+        //}
+
+        public string ObterNovel()
+        {
+            var novel = _driver.FindElement(By.ClassName("most-popular-bxslider"));
+            return novel.Text;
+        }
+
+        public void Fechar()
+        {
+            _driver.Quit();
+            _driver = null;
+        }
+    }
+}
---
+++ SeleniumConfigurations.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teste
+{
+  public  class SeleniumConfigurations
+    {
+        public string CaminhoDriverChrome { get; set; } = @"C:\Selenium";
+        public string UrlPagina { get; set; }
+        public int Timeout { get; set; }
+    }
+}
