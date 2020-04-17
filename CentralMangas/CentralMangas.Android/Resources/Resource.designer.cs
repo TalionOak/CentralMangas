0673325828b72d5ef126b4db8e8e67bf925e35b9
AiltonSilvaSC <ailtonsscarvalho@gmail.com>
2020-04-17 08:26:41 -0300

    movido pasta

--- CentralMangas/CentralMangas.Android/Resources/Resource.designer.cs
+++ CentralMangas/CentralMangas.Android/Resources/Resource.designer.cs
@@ -11926,6 +11926,9 @@ namespace CentralMangas.Droid
 			// aapt resource value: 0x7F0C0002
 			public const int launcher_foreground = 2131492866;
 			
+			// aapt resource value: 0x7F0C0003
+			public const int Logo512x512 = 2131492867;
+			
 			static Mipmap()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
